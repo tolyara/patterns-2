eb176f73f2ae7749832c26ed02b0e485695661ed
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-12 22:34:37 +0200

    Decorator improvements;

--- src/structural/decorator/DecoratorMain.java
+++ src/structural/decorator/DecoratorMain.java
@@ -10,9 +10,18 @@ public class DecoratorMain {
 	public static void main(String[] args) throws FileNotFoundException {
 		YouTubeStreamer streamer = new YouTubeStreamer();
 		streamer.holdStream();
+
 		System.out.println();
 		AdvancedYouTubeStreamer advancedStreamer = new AdvancedYouTubeStreamer(new YouTubeStreamer());
 		advancedStreamer.holdStream();
+
+		System.out.println();
+		ExperiencedYouTubeStreamer experiencedStreamer = new ExperiencedYouTubeStreamer(new YouTubeStreamer());
+		experiencedStreamer.holdStream();
+
+		System.out.println();
+		ExperiencedYouTubeStreamer superExperiencedStreamer = new ExperiencedYouTubeStreamer(new AdvancedYouTubeStreamer(new YouTubeStreamer()));
+		superExperiencedStreamer.holdStream();
 	}
 	
 }
@@ -23,7 +32,8 @@ interface Streamer {
 	
 }
 
-class YouTubeStreamer implements Streamer {
+// use case - the class is final and we cannot extend from it
+final class YouTubeStreamer implements Streamer {
 	
 	@Override
 	public void holdStream() {
@@ -61,6 +71,24 @@ class AdvancedYouTubeStreamer extends StreamerDecorator {
 		gatherDonuts();
 	}	
 	
+}
+
+class ExperiencedYouTubeStreamer extends StreamerDecorator {
+
+	public ExperiencedYouTubeStreamer(Streamer streamer) {
+		super(streamer);
+	}
+
+	void makeAdvertisingIntegration() {
+		System.out.println("Make advertising integration...");
+	}
+
+	@Override
+	public void holdStream() {
+		streamer.holdStream();
+		makeAdvertisingIntegration();
+	}
+
 }
 
 
